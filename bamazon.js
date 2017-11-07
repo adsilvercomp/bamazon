07acc8cdf2c4fc9e6f7b15af1bedd6cf04f836fc
Aaron Silverstein <adsilvercomp@Aarons-MBP.fios-router.home>
2017-11-07 16:18:18 -0500

    update

--- bamazon.js
+++ bamazon.js
@@ -37,8 +37,9 @@ function start() {
                         for (var i = 0; i < results.length; i++) {
                             //information from two database columns will be pushed into the choiceArr
                             //this might make referencing the database difficult.
-                            choiceArray.push(results[i].product_name + " - Price: " + results[i].price);
-                    
+                            choiceArray.push(results[i].product_name);
+                            // " - Price: " + results[i].price
+
                         }
                         return choiceArray;
                     },
@@ -51,20 +52,47 @@ function start() {
                 }
             ])
             .then(function (answer) {
-                var Item=answer.items;
-                console.log(Item);
-                var Amount=answer.amount;
-                console.log(Amount)
-                
+                // var Item = answer.items;
+                // console.log(Item);
+                // var Amount = answer.amount;
+                // console.log(Amount)
+                inventoryUpdate();
+
                 //subtract the orderAmount from the inventory of the selected item
-                function inventoryUpdate(){
-                    //loop through your database
+                function inventoryUpdate() {
+                    var chosenProduct;
+                    //if the chosen item matches an item in the database and the order amount is less than the 
+                    //amount the databse has in stock, update the database by subtracting the order amount from the inventory
 
-                    //if the user's order matches an item in the database, update that item
+                    //query the database for all product names
+                    connection.query("SELECT * FROM products", function (err, results) {
+                        if (err) throw err;
+                        //loop through the product names
+                        var match=false;
+                        for (var i = 0; i < results.length; i++) {
+                            //if the user's order matches the item name & the order amount doesnt exceed the items in stock
+                            if (answer.items === results[i].product_name && answer.amount<results[i].stock_quantity) {
+                                
+                                match=true;
+                                console.log(results[i].product_name + " " + match);
+                                break;
+                                //update the database*****
+                           
+                            
 
-                    //if their are no items left, tell the user we are currently out of stock.
-                    //if the user's order amount exceeds what is available tell the user (we only have x items left)
-                    //else, complete their order
+                                //if match is true query the database for 
+                                //if the order amount exceeds the items in stock console.log("We only have x items in stock")
+
+                                //if the items in stock = 0 console.log("this item is out of stock")
+                            }else if(answer.amount>results[i].stock_quantity){
+                                        break;
+                                        console.log("I'm sorry, we don have that many in stock");
+                                    }else if(results[i].stock_quantity=0){
+                                        break;
+                                        console.log("I'm sorry, but we are out of this item");
+                                    }
+                        }
+                    });
 
                 }
             });
